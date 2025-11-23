6d7e802ea683e38ea7da4de2b4f084e299641cf2
Leandro Olczyk <73176252+LeandroOlczyk@users.noreply.github.com>
2025-11-23 18:21:50 +0000

    Atualização

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-  return <h1>Oi, minha gostosa! ❤️</h1>
+function Home() {
+  return <h1>Oi, minha gostosa! ❤️</h1>;
+}
+
+function teste1() {
+  console.log("teste");
 }
 
 export default Home;
