014f52f59828c2a14babb885513be1ab92081616
Leandro Olczyk <73176252+LeandroOlczyk@users.noreply.github.com>
2025-11-30 15:26:31 +0000

    Atualização

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 function Home() {
-  return <h1>Oi, minha gostosa! ❤️</h1>;
-}
+  return (
+    <>
+      <div className="container">
+        <img
+          className="imagem"
+          src="https://pro-leandro-olczyk.s3.us-east-1.amazonaws.com/public-files/imagens/em-construcao/construction.png"
+          alt="Página em construção"
+        />
+      </div>
 
-function teste1() {
-  console.log("teste");
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+          gap: 20px;
+        }
+
+        .imagem {
+          width: 30%;
+          height: auto;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default Home;
